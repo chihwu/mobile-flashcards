0d55056c0a6edadc7b2a77dcdfd4700c7a0e316a
Chihyung Wu <chihwu@bu.edu>
2019-07-23 19:20:10 -0400

    update -v2

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 class AddCard extends Component {
 
 	render() {
 		return (
-			<View>
-				<Text>Add Card</Text>
+			<View style={ styles.container }>
+				<TextInput placeholder="Quiz Title" maxLength={70} style={ styles.textInput } />
+
+				<TextInput placeholder="Quiz Content" maxLength={70} style={ styles.textInput } />
+
+				<TouchableOpacity style={ styles.button }> 
+					<Text>Submit</Text>
+				</TouchableOpacity>
 			</View>
 		)
 	}
 }
 
 export default AddCard
+
+const styles = StyleSheet.create({
+	container: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingTop: 100
+	},
+	button: {
+		marginTop: 15,
+		paddingLeft: 40,
+		paddingRight: 40,
+		paddingTop: 15,
+		paddingBottom: 15,
+		justifyContent: 'center',
+		borderColor: 'black',
+		borderWidth: 2,
+		borderRadius: 5
+	},
+	textInput: {
+		width: 350,
+		height: 60,
+		marginTop: 10,
+	    marginBottom: 10,
+	    padding: 10,
+	    paddingTop: 5,
+	    paddingBottom: 5,
+	    borderWidth: 1,
+	    borderRadius: 5,
+	    borderColor: 'gray'
+	}
+})
--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 class AddDeck extends Component {
 	render() {
 		return (
-			<View>
-				<Text>Add Deck Form</Text>
+			<View style={ styles.container }>
+				<Text style={ styles.title }>What is the title of your new deck?</Text>
+
+				<TextInput placeholder="Deck Title" maxLength={70} style={ styles.textInput } />
+
+				<TouchableOpacity style={ styles.button }> 
+					<Text>Submit</Text>
+				</TouchableOpacity>
 			</View>
 		)
 	}
 }
 
 export default AddDeck
+
+const styles = StyleSheet.create({
+	container: {
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title: {
+		marginTop: 100,
+		marginBottom: 40,
+		fontSize: 40,
+		fontWeight: 'bold'
+	},
+	button: {
+		marginTop: 15,
+		paddingLeft: 40,
+		paddingRight: 40,
+		paddingTop: 15,
+		paddingBottom: 15,
+		justifyContent: 'center',
+		borderColor: 'black',
+		borderWidth: 2,
+		borderRadius: 5
+	},
+	textInput: {
+		width: 350,
+		height: 60,
+		marginTop: 10,
+	    marginBottom: 10,
+	    padding: 10,
+	    paddingTop: 5,
+	    paddingBottom: 5,
+	    borderWidth: 1,
+	    borderRadius: 5,
+	    borderColor: 'gray'
+	}
+})
